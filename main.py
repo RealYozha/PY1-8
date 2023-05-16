c2dc4b47a7d731416ee158e59f226540306ab079
False <ItzYozha@ya.ru>
2023-05-16 16:48:21 +0300

    v1.2 fixed

--- main.py
+++ main.py
@@ -4,26 +4,24 @@ import random
 all_characters = list(string.printable.replace(string.whitespace, ""))
 status = False
 
+password_len = 0
 
-password_len = int(input("Какой длинны ты хочешь пароль? (7-20)"))
 while password_len > 20 or password_len < 7:
+    password_len = int(input("Какой длинны ты хочешь пароль? (7-20)"))
     if password_len < 20 and password_len > 7:
         print("OK.")
     elif password_len > 20 or password_len < 7:
         print("Такая длинна пароля не подходит.")
-        password_len = int(input("Какой длинны ты хочешь пароль? (7-20)"))
     else:
         print("Я тебя не понимаю.")
-        password_len = int(input("Какой длинны ты хочешь пароль? (7-20)"))
 
 while status == False:
-    password = list([random.choice(all_characters) for i in range(password_len)])
-    print("Пароль:", "".join(password))
+    password = "".join(list([random.choice(all_characters) for i in range(password_len)]))
+    print("Пароль:", password)
     answer = input("Вам нравится этот пароль? (да/нет)")
     if answer == "да":
         status = True
-        print("Отлично! Ваш пароль:",  "".join(password))
-        break
+        print("Отлично! Ваш пароль:", password)
     elif answer == "нет":
         print("Хорошо, меняю")
     else:
